fdc5c61d97f423961e25f32cf56beb652f160793
seancrawford55 <seancrawford@Mac.attlocal.net>
2024-11-24 14:09:06 -0600

    First commit

---
+++ Test_Project/Test_Project/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Test_Project
+//
+//  Created by Sean Crawford on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test_Project/Test_ProjectTests/Test_ProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  Test_ProjectTests.swift
+//  Test_ProjectTests
+//
+//  Created by Sean Crawford on 11/24/24.
+//
+
+import Testing
+
+struct Test_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
